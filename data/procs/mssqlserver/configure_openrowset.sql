541761df32359fa32af11825ef3ba5c20310f4d6
Hitachi-VLLTT <73029410+Hitachi-VLLTT@users.noreply.github.com>
2021-01-09 19:31:24 -0500

    Create configure_openrowset.sql

---
+++ data/procs/mssqlserver/configure_openrowset.sql
@@ -0,0 +1,7 @@
+EXEC master..sp_configure 'show advanced options', 1;
+RECONFIGURE WITH OVERRIDE;
+EXEC master..sp_configure 'Ad Hoc Distributed Queries', %ENABLE%;
+RECONFIGURE WITH OVERRIDE;
+EXEC sp_configure 'show advanced options', 0;
+RECONFIGURE WITH OVERRIDE
+© 2021 GitHub, Inc.
